5ea67532d1cc2a893c7e20928add383fdb190f68
khangng72 <khangnguyn07022002@gmail.com>
2025-10-05 10:05:07 +0700

    initial commit

---
+++ backend/solontax-g1-management/src/test/java/solontax/g1/management/SolontaxG1ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package solontax.g1.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolontaxG1ManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
