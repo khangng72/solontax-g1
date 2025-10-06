12e5ae1da0442206ace3d36e43092059d5fe5574
khangng72 <khangnguyn07022002@gmail.com>
2025-10-06 10:50:29 +0700

    feat: add more exception handler

--- backend/solontax-g1-management/src/main/java/solontax/g1/management/exception/GlobalExceptionHandler.java
+++ backend/solontax-g1-management/src/main/java/solontax/g1/management/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package solontax.g1.management.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(CommonException.class)
     public ResponseEntity<CommonExceptionDto> handleException(CommonException exception) {
@@ -17,16 +19,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(exceptionDto, exception.getStatus());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<CommonExceptionDto> handleUnexpectedException(Exception exception) {
-        CommonExceptionDto exceptionDto =
-                CommonExceptionDto.builder()
-                                  .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                  .message("An unexpected error occurred: " + exception.getMessage())
-                                  .build();
-        return new ResponseEntity<>(exceptionDto, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<CommonExceptionDto> handleIllegalArgument(IllegalArgumentException exception) {
         CommonExceptionDto exceptionDto =
@@ -35,5 +27,39 @@ public class GlobalExceptionHandler {
                                   .message(exception.getMessage())
                                   .build();
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingPathVariableException.class)
+    public ResponseEntity<CommonExceptionDto> handleMissingPathVariableException(MissingPathVariableException exception) {
+        CommonExceptionDto exceptionDto =
+                CommonExceptionDto.builder()
+                                  .status(HttpStatus.BAD_REQUEST)
+                                  .message("Required path variable is missing " + exception.getVariableName())
+                                  .build();
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<CommonExceptionDto> handleNoHandlerFoundException(NoHandlerFoundException exception) {
+        CommonExceptionDto exceptionDto =
+                CommonExceptionDto
+                        .builder()
+                        .status(HttpStatus.NOT_FOUND)
+                        .message(
+                                exception.getMessage() != null ?
+                                        exception.getMessage() :
+                                        "No handler found exception")
+                        .build();
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonExceptionDto> handleGenericException(Exception exception) {
+        CommonExceptionDto exceptionDto =
+                CommonExceptionDto.builder()
+                                  .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                  .message("An unexpected error occurred: " + exception.getMessage())
+                                  .build();
+        return new ResponseEntity<>(exceptionDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
